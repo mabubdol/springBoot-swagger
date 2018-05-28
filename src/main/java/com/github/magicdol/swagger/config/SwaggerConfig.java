0f72ca754fd191473639691b9343c60c65ed5659
김영호 <xfile0304@gmail.com>
2018-05-28 14:03:43 +0900

    Swagger를 사용하여 tomcat에 배포할때 swagger-ui.html 접근시 403 에러가 발생할 경우 해결 방법

--- src/main/java/com/github/magicdol/swagger/config/SwaggerConfig.java
+++ src/main/java/com/github/magicdol/swagger/config/SwaggerConfig.java
@@ -2,7 +2,12 @@ package com.github.magicdol.swagger.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,7 +16,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
@@ -19,6 +24,24 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.github.magicdol"))
                 .paths(regex("/api.*"))
                 .build()
-                ;
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot REST API")
+                .description("\"Spring Boot REST API for Online Store\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"))
+                .build();
+    }
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
